e98f735d1f9743199563f03e1721e89438928522
Nguyen Trong Khoi <ntkdz28102005@gmail.com>
2024-10-28 21:28:05 +0100

    feat: add 2 files at 29th Oct

---
+++ task1_28_10.java
@@ -0,0 +1,27 @@
+package java_cmu;
+
+import java.util.Scanner;
+
+public class task1_28_10 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // TODO code application logic here
+        String myClass;
+        String myName;
+        int myId;
+
+        System.out.println("Enter your Class: ");
+        myClass = scanner.nextLine();
+        System.out.println("Enter your Name: ");
+        myName = scanner.nextLine();
+        System.out.println("Enter your ID: ");
+        myId = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println("CLass: " + myClass);
+        System.out.println("Name: " + myName);
+        System.out.println("ID: " + myId);
+    }
+}
