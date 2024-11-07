ec4060c0ac617d02857da986343f0203f2f38716
Nguyen Trong Khoi <ntkdz28102005@gmail.com>
2024-11-07 14:06:08 +0000

    push all

---
+++ Array/Assignment5/FindLargestAndSmallest.java
@@ -0,0 +1,31 @@
+
+package java_cmu.Array.Assignment5;
+import java.util.Scanner;
+public class FindLargestAndSmallest {
+    public static void findMaxMin(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter quantity element:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0 ; i < arr.length ; i++){
+            System.out.print("Enter value index " + i + ":");
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+        for(int i = 1 ; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        System.out.println("Largest of Array: " + max);
+        System.out.println("Smallest of Array: " + min);
+    }
+    public static void main(String[] args) {
+        FindLargestAndSmallest.findMaxMin();
+    }
+}
